492bda7721ca54147d135743088cd9ee9fadfcfe
Luis Echegaray <mrluisechegaray@gmail.com>
2016-01-09 09:00:40 -0500

    meteor with react example initial commit

---
+++ Task.jsx
@@ -0,0 +1,12 @@
+// Task component - represents a single item
+
+Task = React.createClass({
+  propTypes: {
+    task: React.PropTypes.object.isRequired
+  },
+  render() {
+    return (
+      <li>{this.props.task.text}</li>
+    );
+  }
+});
